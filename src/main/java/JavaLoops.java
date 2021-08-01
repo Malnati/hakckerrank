f21ac691758d6b7bf60feadf547e179513b7ee15
Ricardo Malnati <ricardomalnati@gmail.com>
2021-07-31 21:18:39 -0300

    feat(introduction): java loops II

---
+++ src/main/java/JavaLoops.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/**
+ * This is a solution for Java Loops II challenge of hackerrank.com
+ * 
+ * @see https://www.hackerrank.com/rest/contests/master/challenges/java-loops/hackers/ricardomalnati/download_solution
+ * @author ricardomalnati
+ *
+ */
+public class JavaLoops {
+
+	public static void reducer (int start, int constant, int exp, int index, int limit, int accumulator) {
+		if (index <= limit) {
+			int nextValue = exp * constant + accumulator;
+			System.out.print(String.format("%d ", start + nextValue));
+			reducer(start, constant, 2* exp, ++index, limit, nextValue);
+		}
+	}
+	
+	public static void main(String []args){
+		
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+        	String line = in.nextLine();
+        	if(! line.isEmpty() ) {
+        		System.out.println("line: " + line);
+        		String[] values = line.split("\\s");
+        		
+        		if ( values.length == 1 || values.length == 3 ) {
+        			
+        			System.out.println("values.length: " + values.length);
+
+        			int start = Integer.parseInt(values[0]);
+        			System.out.println("start: " + start);
+
+        			if ( values.length == 3 ) {
+        				int constant = Integer.parseInt(values[1]);
+        				System.out.println("constant: " + constant);
+        				
+        				int limit = Integer.parseInt(values[2]);
+        				System.out.println("limit: " + limit);
+        				
+    					reducer(start, constant, 1, 1, limit, 0);
+        			}
+        		}
+        	}
+        }
+        in.close();
+    }
+
+}
